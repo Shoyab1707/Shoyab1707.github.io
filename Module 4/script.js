7abcf4126ba4ddd1bcec7156375012a2c51edd8a
Shoyab1707 <95013720+Shoyab1707@users.noreply.github.com>
2022-07-16 18:48:09 +0530

    updated

--- Module 4/script.js
+++ Module 4/script.js
@@ -1,14 +1,16 @@
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'j'){
-    console.log("GoodBye "+ names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
   }
   else {
-    console.log("Hello " +names[i]);
+    helloSpeaker(names[i]);
   }
 }
 
+})();
